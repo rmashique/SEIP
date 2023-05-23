c3cb7c052a7afb90a8819761e9e2e67b77228f26
Najmul-Sarker <najmulsarker.cse@gmail>
2023-05-23 13:47:47 +0600

    seip project

---
+++ admin/layout_1/LTR/material/full/slider_index_print.php
@@ -0,0 +1,60 @@
+<!-- <?php include_once($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'config.php') ?>
+<?php
+
+    $dataSlides = file_get_contents($datasource.DIRECTORY_SEPARATOR.'slideritems.json');
+    $slides = json_decode($dataSlides);
+   
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<h1> All Sliders </h1>
+
+<table border="1">
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Title</th>
+									<th>Src</th>
+									<th>Alt</th>
+									<th>Caption</th>
+									
+								</tr>
+							</thead>
+							<tbody>
+
+<?php
+    foreach($slides as $key=>$slide):
+        // if(0 == $key){
+        //  $active = 'active';
+        // }else{
+        //  $active = '';
+        // }
+		
+     ?> 
+
+
+								<tr>
+									<td title="<?=$slide->uuid?>"><?=++$key?></td>
+									<td><?=$slide->title?></td>
+									<td><img src="<?=$webportal."uploads/".$slide->src?>" style="width:100px;height:100px"></td>
+									<td><?=$slide->alt?></td>
+									<td><?=$slide->caption?></td>
+									
+								</tr>
+<?php
+ endforeach
+?>
+							</tbody>
+						</table>
+</body>
+</html>
+ -->
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'config.php');
+?>
+
+<ul>
+
+    <li>Go to Portal<a href=<?php echo $adminurl."app_dashboard.php" ?>>Admin</a></li>
+    <li>Go to Portal<a href=<?php echo $frontendurl."frontend_index.php" ?>>frontend</a></li>
+    <li>Go to Portal</li>
+</ul>
